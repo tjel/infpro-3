82485e9d4fbbb77a3668f9a08b886cc6d3961a2a
tjel <tomek.jelinski@gmail.com>
2015-10-08 11:23:31 +0200

    Add area directory.

---
+++ area/area.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+
+using namespace std;
+
+int main( ) 
+{ 
+    float PI = 3.14;                // variables can be initialized during declaration 
+    int rad; 
+    cout<< "Enter the radius"; 
+    cin>>rad; 
+    cout<< "Area of the circle is "<< PI * rad * rad; 
+    return 0;
+}  
